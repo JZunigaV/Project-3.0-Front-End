e9d7f7a1590d4104f87ad9f7934ab1f5df04f8e1
JzunigaV <j_zuniga4@hotmail.com>
2019-04-03 18:13:01 -0600

    added conema links we need to manage conepolis still

--- project30-front/src/components/Profile/InfoModal.jsx
+++ project30-front/src/components/Profile/InfoModal.jsx
@@ -23,6 +23,8 @@ class InfoModal extends Component {
   providers = () => {
     let providers = this.state.movieInfo.offers;
 
+    debugger;
+
     if (providers) {
       let iconsUrl = [];
       //Remove duplicate providers
@@ -32,12 +34,24 @@ class InfoModal extends Component {
       );
 
       uniqueProviders.map((ele, index) => {
+        if (ele.monetization_type === "cinema") {
+          let cine = ele.urls.standard_web.substr(8, 7);
+          let newObj = { ...ele };
+          if (cine === "cinemex") {
+            newObj.provider_id = 1200;
+            ele = newObj;
+          }
+        }
+
+        debugger;
+
         switch (ele.provider_id) {
           case 8:
             let netflix = {
               name: "Netflix",
               iconUrl: "https://images.justwatch.com/icon/430997/s100/",
               link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
             };
             iconsUrl.push(netflix);
             break;
@@ -47,6 +61,7 @@ class InfoModal extends Component {
               name: "ClaroVideo",
               iconUrl: "https://images.justwatch.com/icon/9899714/s100/",
               link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
             };
             iconsUrl.push(claro);
             break;
@@ -56,6 +71,7 @@ class InfoModal extends Component {
               name: "HBO GO",
               iconUrl: "https://images.justwatch.com/icon/614494/s100/",
               link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
             };
             iconsUrl.push(hbo);
             break;
@@ -65,6 +81,7 @@ class InfoModal extends Component {
               name: "Amazon Prime Video",
               iconUrl: "https://images.justwatch.com/icon/52449861/s100/",
               link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
             };
             iconsUrl.push(amazon);
             break;
@@ -74,6 +91,7 @@ class InfoModal extends Component {
               name: "YouTube Premium",
               iconUrl: "https://images.justwatch.com/icon/70189310/s100/",
               link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
             };
             iconsUrl.push(youtube);
             break;
@@ -83,6 +101,7 @@ class InfoModal extends Component {
               name: "Apple iTunes",
               iconUrl: "https://images.justwatch.com/icon/430995/s100/",
               link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
             };
             iconsUrl.push(apple);
             break;
@@ -92,6 +111,7 @@ class InfoModal extends Component {
               name: "Google Play Movies",
               iconUrl: "https://images.justwatch.com/icon/430996/s100/",
               link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
             };
             iconsUrl.push(google);
             break;
@@ -101,28 +121,52 @@ class InfoModal extends Component {
               name: "Microsoft Store",
               iconUrl: "https://images.justwatch.com/icon/820542/s100/",
               link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
             };
             iconsUrl.push(microsoft);
+            break;
+
+          case 1200:
+            let cinemex = {
+              name: "Cinemex",
+              iconUrl:
+                "https://is1-ssl.mzstatic.com/image/thumb/Purple114/v4/ec/99/ca/ec99ca66-ef8e-e39f-e747-cd0fb3645b5b/AppIcon-1-0-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-220-0-0-0-5.png/246x0w.jpg",
+              link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
+            };
+
+            let cinepolis = {
+              name: "Cinepolis",
+              iconUrl:
+                "https://www.google.com/search?biw=1016&bih=486&tbm=isch&sa=1&ei=MEqlXNfhIIaWsgW_8oHYBg&q=cinepolis+icon&oq=cinepolis+icon&gs_l=img.3..35i39j0.2022.4243..4403...0.0..0.83.928.14......1....1..gws-wiz-img.....0..0i67j0i5i30j0i8i30j0i10i24.793zeJqxKtc#imgrc=OdSttiYvHUuuBM:",
+              link: ele.urls.standard_web,
+              movieUrl: ele.urls.standard_web,
+            };
+
+            iconsUrl.push(cinemex, cinepolis);
             break;
 
           default:
             break;
         }
+
         return iconsUrl;
       });
 
       return (
         <>
           {iconsUrl.map(offer => (
-            <Button
-              key={offer.name}
-              className="btn-providers"
-              color="default"
-              href="#pablo"
-              onClick={e => e.preventDefault()}
-            >
-              <img alt="..." src={offer.iconUrl} />
-            </Button>
+            <>
+              <Button
+                key={offer.name}
+                className="btn-providers"
+                color="default"
+                href={offer.movieUrl}
+                onClick={e => e.preventDefault()}
+              >
+                <img alt="..." src={offer.iconUrl} />
+              </Button>
+            </>
           ))}
         </>
       );
